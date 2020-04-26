655bbe859d289596504515820a365420a4f0248e
Benjamin Lumbye <benjamin@lumbye.net>
2020-04-26 17:30:37 +0200

    Revert "Merge branch 'master' of https://github.com/BLumbye/Underworld"
    This reverts commit 48dc0e11f9ba55bf396fdcd5b1b91c8ff5fad2fc, reversing
    changes made to 67a7a968b1f5f73c3a6a132e723dce8b1df3514a.

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,21 @@
+﻿using DG.Tweening;
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+    [Scene]
+    [SerializeField] public int gameScene;
+
+    void Start() {
+        DOTween.Init(true, true, LogBehaviour.Default);
+    }
+
+    public void Play() {
+        SceneManager.LoadScene(gameScene);
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+}
