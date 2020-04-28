0795c31cf482664770b7916fed390803442659f0
Benjamin Lumbye <benjamin@lumbye.net>
2020-04-28 21:43:49 +0200

    Logo

---
+++ Assets/LogoGlow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class LogoGlow : MonoBehaviour {
+    [ColorUsage(true, true)]
+    [SerializeField] private Color emissionColor;
+
+    // Start is called before the first frame update
+    void Start() {
+        MaterialPropertyBlock propertyBlock = new MaterialPropertyBlock();
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        sr.GetPropertyBlock(propertyBlock);
+        propertyBlock.SetColor("_EmissionColor", emissionColor);
+        sr.SetPropertyBlock(propertyBlock);
+    }
+}
